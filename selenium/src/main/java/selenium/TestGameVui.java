6678964bc5871b020cb7bb528d33a9cf044de759
AnhTuanIT <ngocdungbb1@gmail.com>
2021-04-25 22:05:32 +0700

    done

--- selenium/src/main/java/selenium/TestGameVui.java
+++ selenium/src/main/java/selenium/TestGameVui.java
@@ -1,11 +1,13 @@
 package selenium;
 
+import java.io.File;
 import java.util.concurrent.TimeUnit;
+import java.util.regex.Pattern;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.openqa.selenium.chrome.ChromeOptions;
 
 import io.github.bonigarcia.wdm.DriverManagerType;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -14,10 +16,11 @@ public class TestGameVui {
 	public static void main(String[] args) {
 		WebDriver driver = null;
 		try {
-			// đang test chưa chạy okay lắm
 			DriverManagerType chrome = DriverManagerType.CHROME;
 			WebDriverManager.getInstance(chrome).setup();
-			driver = new ChromeDriver();
+			ChromeOptions options = new ChromeOptions();
+			options.setExperimentalOption("excludeSwitches", new String[]{"enable-automation"});
+			driver = new ChromeDriver(options);
 			
 			//wait 3s before throw exception
 			driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
@@ -28,13 +31,15 @@ public class TestGameVui {
 			//click đăng nhập
 			driver.findElement(By.xpath("//a[contains(text(),'Đăng nhập')]")).click();
 			
-			Thread.sleep(30000);
+			//skip quảng cáo
+			driver.manage().window().maximize();
 			
 			//type "phanmotau" input email
-			driver.findElement(By.xpath("//input[@id='UserName']")).sendKeys("phanmotau");;
+			driver.findElement(By.xpath("//input[@id='UserName']")).sendKeys("phanmotau");
 			
 			//type password
-			driver.findElement(By.xpath("//input[@id='Password']")).sendKeys("caosuhu");
+			driver.findElement(By.xpath("//input[@id='Password']")).sendKeys(hash("u1h2u3s4o5ac6"));
+			
 			
 			//click button đăng nhập
 			driver.findElement(By.xpath("//button[contains(text(),'Đăng nhập')]")).click();
@@ -42,17 +47,19 @@ public class TestGameVui {
 			String username = driver.findElement(By.xpath("//span[contains(text(),'phanmotau')]")).getText();
 			
 			if(username.equals("phanmotau")) {
-				System.out.println("true");
-			} else {
-				System.out.println("false");
+				System.out.println("check okay");
 			}
-			
-			
+
 		} catch (Exception e) {
-			System.out.println("Error");
+			System.out.println("check fail");
 		} finally {
 			driver.close();
 		}
+	}
+	
+	public static String hash(String password) {
+		Pattern pattern = Pattern.compile("[^a-zA-Z]");
+		return new StringBuilder(String.join("", pattern.split(password))).reverse().toString();
 	}
 	
 }
